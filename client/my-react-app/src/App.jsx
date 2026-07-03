5b92f416468b17159ef8b284922e6632f90c3648
Aryan Rustagi <aryanrustagi1234@gmail.com>
2026-07-03 12:08:43 +0530

    SignUp

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
      <Routes>
       <Route path='/' element={<LandingPage/>}/>
       <Route path='/Login' element={<Login/>}/>
+      <Route path='/SignUp' element={<SignUp/>}/>
      </Routes>
 
      </BrowserRouter>
--- client/my-react-app/src/pages/SignUp.jsx
+++ client/my-react-app/src/pages/SignUp.jsx
@@ -1,1 +1,8 @@
 //SignUp
+function SignUp(){
+    return(
+        <h1 color="red" >Wow already login now please signup</h1>
+    )
+}
+
+export default SignUp
