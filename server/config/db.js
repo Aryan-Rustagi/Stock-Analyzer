4e6ef70a4695311d0d95f9b785dcd500f1277631
Aryan Rustagi <aryanrustagi123@gmail.com>
2026-07-09 11:50:26 +0530

    database-connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,20 @@
-const connectDB = async () => {
-	return null;
-};
 
-module.exports = connectDB;
+const mongoose= require('mongoose');
+
+async function connectDb(){
+	try{
+	  if(!process.env.MONGO_URI){
+		throw new Error("MONGO_URI is not defined");
+	  }
+
+	  await mongoose.connect(process.env.MONGO_URI);
+	  console.log("Connected to database");
+	}
+	catch(error){
+		console.log("Error: ",error.message);
+		process.exit(1);
+
+	}
+}
+
+module.exports=connectDb;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const express= require('express');
 const cors=require('cors');
+const dotenv=require('dotenv');
+const connectDb=require('./config/db.js')
+
+dotenv.config();
 
 const app=express();
 
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/",function(req,res){
     res.send("Welcome to the server ");
 });
 
-app.listen(PORT,function(){
-    console.log("Server is running ");
+async function startServer(){
+    await connectDb();
+
+    app.listen(PORT,function(){
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+startServer().catch(function(error){
+    console.log("Server failed to start:", error.message);
+    process.exit(1);
 });
