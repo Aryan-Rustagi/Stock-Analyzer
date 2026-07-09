99e05031490820ac6f0aff9cb1afd8907deb3b0d
Aryan Rustagi <aryanrustagi123@gmail.com>
2026-07-09 18:01:09 +0530

    Routing

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,14 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-async function connectDb(){
-	try{
-<<<<<<< HEAD
-		const conn= await mongoose.connect(process.env.MONGO_URI);
+async function connectDb() {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-		console.log("Connected to database");
-=======
-
-	  const conn=await mongoose.connect(process.env.MONGO_URI);
-	  console.log("Connected to database");
->>>>>>> 80b0f5e (Router)
-	}
-	catch(error){
-		console.log( console.error("MongoDB connection failed:", error.message));
-		process.exit(1);
-	}
-};
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+}
 
 module.exports = connectDb;
