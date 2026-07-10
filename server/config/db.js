74e116fceffcfd0ffd5cef193cdb1a22fb5d746a
Aryan Rustagi <aryanrustagi1234@gmail.com>
2026-07-10 20:27:14 +0530

    Login-Signup

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-async function connectDb() {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+async function connectDb(){
+	try{
+		const conn= await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("Connected to database");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
-  }
-}
+		console.log("Connected to database");
+	}
+	catch(error){
+		console.log( console.error("MongoDB connection failed:", error.message));
+		process.exit(1);
+	}
+};
 
 module.exports = connectDb;
